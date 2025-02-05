7aafc3084c56ab599d15f3d807d4ea266b9eac3b
Thoby Jim Ralleta <152825042+thobidogs@users.noreply.github.com>
2025-02-05 08:57:30 +0000

    Done

--- GameAppDemo.java
+++ GameAppDemo.java
@@ -1,26 +1,37 @@
 public class GameAppDemo {
     public static void main (String[] args){
         
-        Character knight = new Knight();
-        Character wizard = new Wizard();
-        Character archer = new Archer();
+        GameApp knightApp = new GameApp();
+        knightApp.setChr(new Knight());
+        knightApp.setAtk(new SwingSword());
+        knightApp.attack();
 
-        GameApp knightApp = new GameApp(knight);
-        knightApp.attack(new SwingSword());
-        
-        GameApp wizardApp = new GameApp(wizard);
-        wizardApp.attack(new CastSpell());
+        GameApp wizardApp = new GameApp();
+        wizardApp.setChr(new Wizard());
+        wizardApp.setAtk(new CastSpell());
+        wizardApp.attack();
 
-        GameApp archerApp = new GameApp(archer);
-        archerApp.attack(new ShootArrow());
+        GameApp archerApp = new GameApp();
+        archerApp.setChr(new Wizard());
+        archerApp.setAtk(new ShootArrow());
+        archerApp.attack();
 
-        knightApp.defend(new CreateMagic());
-        knightApp.defend(new Dodge());
-        knightApp.defend(new Shield());
+        knightApp.setDef(new Shield());
+        knightApp.defend();
 
-        wizardApp.defend(new CreateMagic());
+        knightApp.setDef(new Dodge());
+        knightApp.defend();
 
-        archerApp.defend(new Shield());
+        knightApp.setDef(new CreateMagic());
+        knightApp.defend();
+
+        wizardApp.setDef(new CreateMagic());
+        wizardApp.defend();
+
+        archerApp.setDef(new Shield());
+        archerApp.defend();
+
+
 
         
     }
